c60bb9cf84e2e6e5adab52c6abfbf2a661e5ddcb
Eric Abouaf <eric.abouaf@gmail.com>
2009-01-16 15:49:07 +0100

    First commit

---
+++ persevere-mysql-autoconfig.js
@@ -0,0 +1,106 @@
+
+/**
+ * CONFIGURATION
+ */
+ 
+var host = "localhost";       // Database host to connect to :
+var database = "yourdbname";  // Database name :
+var username = "dbuser";      // MySQL username & password
+var password = "dbpasswd";
+var utf8 = true;              // Should be true if your database is in UTF8
+
+/**
+ * END OF CONFIGURATION
+ */
+
+
+
+
+
+// The mysql Driver class
+var driver = "com.mysql.jdbc.Driver";
+
+// Output file name
+var destFileName = database+".json";
+
+// Required to generate the json output
+load("lib/json.js");
+// For human-readable json
+load("lib/beautify.js");
+
+
+// Connect to the database
+var connectionStr = "jdbc:mysql://"+host+"/"+database;
+importPackage(java.sql);
+java.lang.Class.forName(driver).newInstance();
+var c = DriverManager.getConnection(connectionStr, username, password);
+
+
+
+function generateSourceFromTable(tableName) {
+   
+   var fieldList = [];
+   var s = c.createStatement();
+   s.executeQuery("SHOW FIELDS FROM "+tableName);
+   var rs = s.getResultSet();
+   while (rs.next()) {
+      var fieldName = rs.getString("Field");
+      if(fieldName != "id") {
+         fieldList.push(fieldName);
+      }
+   }
+   rs.close();
+   
+   return {
+	      "name": tableName,
+	      "sourceClass":"org.persvr.datasource.DatabaseTableDataSource",
+		   "driver": driver,
+		   "connection": connectionStr+"?user="+username+"&pass="+password+(utf8 ? "&useUnicode=true&characterEncoding=utf-8&mysqlEncoding=utf8" : ""),
+		   "table": tableName,
+		   "idColumn":"id",
+		   "dataColumns": fieldList,
+		   "schema":{
+			   "prototype":{},
+			   "data":{"$ref":"../"+tableName+"/"}
+			}
+	};
+   
+};
+
+var configObject = {
+   "id": destFileName,
+   "sources": []
+};
+
+// Generate tableList array
+var tableList = [];
+var tableKey = "Tables_in_"+database;
+var s = c.createStatement();
+s.executeQuery("SHOW TABLES");
+var rs = s.getResultSet();
+while (rs.next()) {
+   var tableName = rs.getString(tableKey);
+   tableList.push(tableName);
+}
+rs.close();
+   
+
+for(var i = 0 ; i < tableList.length ; i++) {
+   configObject.sources.push( generateSourceFromTable(tableList[i]) ); 
+}
+   
+
+
+// Close the database connection
+c.close();
+
+
+// Save the config file
+var destFile = new java.io.File(destFileName);
+destFile.createNewFile(); 
+var fos = new java.io.FileOutputStream(destFile);
+fos.write(  (new java.lang.String("("+js_beautify(configObject.toJSONString())+")")).getBytes() );
+
+
+
+
---
+++ persevere-mysql-autoconfig.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# Run the mysql auto-config script tool for persevere (read the README file !)
+java -classpath ../../lib/mysql-connector-java-5.1.6-bin.jar:../../WEB-INF/lib/js.jar org.mozilla.javascript.tools.shell.Main persevere-mysql-autoconfig.js
